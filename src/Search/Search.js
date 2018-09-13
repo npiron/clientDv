765f4a8e07a7a91be61035eaf899c6014092741d
nicolas PIRON <piron.nicolas@gmail.com>
2018-09-13 21:36:20 +0200

    init React

---
+++ src/Search/Search.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import './Search.css';
+
+class Search extends Component {
+  constructor(props){
+    super();
+    this.state = {
+      
+    };
+  }
+
+  componentWillMount(){}
+  componentDidMount(){}
+  componentWillUnmount(){}
+
+  componentWillReceiveProps(){}
+  shouldComponentUpdate(){}
+  componentWillUpdate(){}
+  componentDidUpdate(){}
+
+  render() {
+    return (
+      <div>
+
+      </div>
+    );
+  }
+}
+
+export default Search;
